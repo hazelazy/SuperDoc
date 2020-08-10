fe4e9e6b8742cdadca6ab309fcde1d62276a3f35
hazelazy <892644680@qq.com>
2020-08-10 17:38:50 +0800

    add Register2/3

--- xxyshh/untitled3/myproject/src/router/index.js
+++ xxyshh/untitled3/myproject/src/router/index.js
@@ -4,6 +4,8 @@ import Home from "../components/Home";
 import App from "../App";
 import HelloWorld from "../components/HelloWorld";
 import Register1 from "../components/Register1";
+import Register2 from "../components/Register2";
+import Register3 from "../components/Register3";
 
 Vue.use(Router)
 
@@ -23,6 +25,16 @@ export default new Router({
       path: '/register1',
       name: 'register1',
       component: Register1
+    },
+    {
+      path: '/register2',
+      name: 'register2',
+      component: Register2
+    },
+    {
+      path: '/register3',
+      name: 'register3',
+      component: Register3
     }
   ]
 })
